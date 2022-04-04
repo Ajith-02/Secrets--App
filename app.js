48e9abfbf5cf4ce0ec118bdba5256cb125aa6dd4
pajith551@gmail.com <pajith551@gmail.com>
2022-04-04 11:34:13 +0530

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const { Collection } = require("mongodb");
 
 
@@ -24,9 +24,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 // creating model and specifying Collection
 const User = new mongoose.model("User", userSchema);
@@ -49,7 +46,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5 (req.body.password)
     });
     newUser.save(function(err){
         if(err){
@@ -63,7 +60,7 @@ app.post("/register", function(req, res){
 // login route
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5 (req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
@@ -82,6 +79,7 @@ app.post("/login", function(req, res){
 app.listen(3000, function(){
     console.log("Server started in Port number 3000");
 });
+
 
 
 

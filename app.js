394c8955ec593a34d0d75c90a4ae8f970e1e5ee5
pajith551@gmail.com <pajith551@gmail.com>
2022-04-06 01:06:57 +0530

    Funishing Up the App -Letting Users Submit Secrets

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -112,12 +113,50 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+// app.get("/secrets", function (req, res) {
+//   if (req.isAuthenticated()) {
+//     res.render("secrets");
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
+
 app.get("/secrets", function (req, res) {
+ User.find({"secret": {$ne: null}}, function(err, foundUsers){
+   if(err){
+     console.log(err);
+   }else{
+     if(foundUsers){
+       res.render("secrets", {usersWithSecrets: foundUsers});
+     }
+   }
+ });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else {
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function (req, res) {
@@ -164,3 +203,5 @@ app.post("/login", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started in Port number 3000");
 });
+
+
